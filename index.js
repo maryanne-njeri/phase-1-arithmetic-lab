125229bde65c2ad69c3c775e8f3297839e21e184
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-07 01:54:32 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 28;
+let num4 = 6;
+
+let multiply = num1 * num2;
+let random = parseInt(Math.random()) + 1;
+let mod = num3 % num4;
+let max = Math.max(0, 15, 3, 20, 18);
